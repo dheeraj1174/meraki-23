6c7634d81918abae0b3a00aac9b95c16654cc53e
Akshat Mittal <akshatmittal2506@gmail.com>
2023-08-04 16:47:47 +0530

    feat: created db models

--- constants/enum.ts
+++ constants/enum.ts
@@ -9,3 +9,14 @@ export const RESPONSE_MESSAGES = {
 	FAILED: "Failed",
 	SERVER_ERROR: "Internal Server Error",
 };
+
+export const TEAM_PARTICIPATION_STATUS = {
+	ACCEPTED: "accepted",
+	PENDING: "pending",
+	REJECTED: "rejected",
+};
+
+export const OTP_TYPES = {
+	REGISTER: "register",
+	RESET_PASSWORD: "reset_password",
+};
