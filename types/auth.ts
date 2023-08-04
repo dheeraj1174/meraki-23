3bc5571d9049e7e2e255fc98fe23ec7b8dc74ca7
Akshat Mittal <akshatmittal2506@gmail.com>
2023-08-04 19:59:33 +0530

    feat: created regex patterns

---
+++ types/auth.ts
@@ -0,0 +1,26 @@
+export interface LoginValues {
+	email: string;
+	password: string;
+}
+
+export interface RegisterValues {
+	name: string;
+	email: string;
+	password: string;
+	confirmPassword?: string;
+	location?: string;
+	phone?: string;
+}
+
+export interface ResetPasswordValues extends LoginValues {
+	confirmPassword: string;
+}
+
+export type USER_ROLES = "admin" | "user" | "guest";
+
+export interface IUser {
+	_id: string;
+	name: string;
+	email: string;
+	role: USER_ROLES;
+}
