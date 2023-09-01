c606cb863f22fd90fed2a0ab79c7c3037478f1cf
Akshat Mittal <akshatmittal2506@gmail.com>
2023-09-01 19:34:32 +0530

    feat: revamped hero section

--- components/home/Hero/index.tsx
+++ components/home/Hero/index.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Navigation } from "..";
-import Typography from "@/library/Typography";
 import styles from "./styles.module.scss";
 import { stylesConfig } from "@/utils/functions";
 
@@ -9,23 +7,10 @@ const classes = stylesConfig(styles, "home-hero");
 const HomeHero: React.FC = () => {
 	return (
 		<section className={classes("")}>
-			<Typography
-				type="heading"
-				variant="display"
-				className={classes("-title")}
-			>
-				Meraki
-			</Typography>
-			<button
-				className={classes("-btn")}
-				type="button"
-				onClick={() => {
-					window.scroll(0, window.innerHeight);
-				}}
-			>
-				<span className={classes("-btn-arrow")} />
-			</button>
-			<Navigation />
+			<div className={classes("-frame")}>
+				<span className={classes("-subtitle")}>the</span>
+				<span className={classes("-title")}>Meraki</span>
+			</div>
 		</section>
 	);
 };
