b0dea4d840c8a7086a473cc1bdeb71a0b02200e8
Akshat Mittal <akshatmittal2506@gmail.com>
2023-09-02 12:04:24 +0530

    feat: revamped account section

--- components/home/Account/index.tsx
+++ components/home/Account/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import useStore from "@/hooks/store";
-import Typography from "@/library/Typography";
 import { USER_ROLES } from "@/constants/enum";
 import Button from "@/library/Button";
 import { useRouter } from "next/router";
@@ -15,48 +14,52 @@ const HomeAccout: React.FC = () => {
 
 	return (
 		<section className={classes("")}>
-			<div className={classes("-name")}>
-				<Typography
-					type="heading"
-					variant="display"
-					className={classes("-name__text")}
-				>
-					{isLoggedIn
-						? `Hii, ${user?.name}`
-						: "Envision the rise of bright tech"}
-				</Typography>
+			<div className={classes("-frame")}>
+				<div className={classes("-eclipse")}>
+					<div className={classes("-layer", "-layer-1")}></div>
+					<div className={classes("-layer", "-layer-2")}></div>
+					<div className={classes("-layer", "-layer-3")}></div>
+					<div className={classes("-layer", "-layer-4")}></div>
+					<div className={classes("-layer", "-layer-5")}></div>
+					<div className={classes("-layer", "-layer-6")}></div>
+				</div>
+				<div className={classes("-container")}>
+					<span className={classes("-title")}>Welcome to MERAKI</span>
+					<span className={classes("-description")}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Ducimus molestiae labore sapiente corrupti explicabo
+						minus illo fugit laudantium illum vitae nemo magnam,
+						molestias voluptate cumque officiis ipsa ullam et
+						placeat!
+						<br />
+						<br />
+						{isLoggedIn ? (
+							<>Hii {user?.name}</>
+						) : (
+							<>Join the Hype!</>
+						)}
+					</span>
+					<Button
+						className={classes("-button")}
+						variant="light"
+						onClick={() => {
+							if (!isLoggedIn) {
+								router.push("/login");
+							} else if (user?.role === USER_ROLES.ADMIN) {
+								router.push("/admin");
+							} else {
+								router.push("/profile");
+							}
+						}}
+					>
+						{!isLoggedIn
+							? "Join us Today!"
+							: user?.role === USER_ROLES.ADMIN
+							? "Explore Admin Dashboard"
+							: "Visit your profile"}
+					</Button>
+				</div>
 			</div>
-			{!isLoggedIn || !user ? (
-				<Button
-					onClick={() => {
-						router.push("/login");
-					}}
-					variant="light"
-					size="medium"
-				>
-					Join the hype
-				</Button>
-			) : user.role === USER_ROLES.ADMIN ? (
-				<Button
-					onClick={() => {
-						router.push("/admin");
-					}}
-					variant="light"
-					size="medium"
-				>
-					Go to your Dashboard
-				</Button>
-			) : (
-				<Button
-					onClick={() => {
-						router.push("/profile");
-					}}
-					variant="light"
-					size="medium"
-				>
-					Go to your Profile
-				</Button>
-			)}
 		</section>
 	);
 };
--- components/home/Events/index.tsx
+++ components/home/Events/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./styles.module.scss";
 import { stylesConfig } from "@/utils/functions";
 import Events from "@/components/Events";
-import Typography from "@/library/Typography";
 import { IEvent } from "@/types/event";
 
 interface HomeEventsProps {
@@ -14,13 +13,6 @@ const classes = stylesConfig(styles, "home-events");
 const HomeEvents: React.FC<HomeEventsProps> = ({ events }) => {
 	return (
 		<section className={classes("")}>
-			<Typography
-				type="heading"
-				variant="display"
-				className={classes("-title")}
-			>
-				Events
-			</Typography>
 			<Events events={events} />
 		</section>
 	);
