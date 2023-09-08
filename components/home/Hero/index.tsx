51670c33ec69d89dcd439afdd9903ad96437253b
Akshat Mittal <akshatmittal2506@gmail.com>
2023-09-08 17:08:31 +0530

    feat: created homepage navbar

--- components/home/Hero/index.tsx
+++ components/home/Hero/index.tsx
@@ -2,13 +2,50 @@ import React from "react";
 import styles from "./styles.module.scss";
 import { stylesConfig } from "@/utils/functions";
 import useStore from "@/hooks/store";
+import Button from "@/library/Button";
+import { USER_ROLES } from "@/constants/enum";
+import Avatar from "@/components/Avatar";
+import useDevice from "@/hooks/device";
+import { useRouter } from "next/router";
 
 const classes = stylesConfig(styles, "home-hero");
 
 const HomeHero: React.FC = () => {
-	const { events } = useStore();
+	const router = useRouter();
+	const { events, user, isLoggedIn } = useStore();
+	const { type: device } = useDevice();
+
 	return (
 		<section className={classes("")}>
+			<header className={classes("-header")}>
+				<Avatar
+					src="/favicon-512.png"
+					alt="Meraki IIITU Logo"
+					size={device === "mobile" || device === "tablet" ? 48 : 36}
+				/>
+				<Button
+					size={
+						device === "mobile" || device === "tablet"
+							? "small"
+							: "medium"
+					}
+					onClick={() => {
+						if (!isLoggedIn) {
+							router.push("/login");
+						} else if (user?.role === USER_ROLES.ADMIN) {
+							router.push("/admin");
+						} else {
+							router.push("/profile");
+						}
+					}}
+				>
+					{!isLoggedIn
+						? "Join us Today!"
+						: user?.role === USER_ROLES.ADMIN
+						? "Go to Admin Dashboard"
+						: `Hii ${user?.name.split(" ")?.[0]} Visit Profile`}
+				</Button>
+			</header>
 			<div className={classes("-carousel")} data-aos="fade-left">
 				{events.map((event) => (
 					<div
