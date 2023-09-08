5de594868ac10c3d656b42ca6f8856c522055443
Akshat Mittal <akshatmittal2506@gmail.com>
2023-09-08 16:53:17 +0530

    feat: revamped events page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -3,9 +3,11 @@ import Typography from "@/library/Typography";
 import { stylesConfig } from "@/utils/functions";
 import { getEvents } from "@/utils/api/events";
 import { IEvent } from "@/types/event";
-import Events from "@/components/Events";
 import styles from "@/styles/pages/Events.module.scss";
 import useStore from "@/hooks/store";
+import Button from "@/library/Button";
+import Responsive from "@/layouts/Responsive";
+import Navigation from "@/components/Navigation";
 
 interface EventsPageProps {
 	events: IEvent[];
@@ -23,6 +25,7 @@ const EventsPage: React.FC<EventsPageProps> = ({ events }) => {
 
 	return (
 		<main className={classes("")}>
+			<Navigation />
 			<Typography
 				type="heading"
 				variant="display"
@@ -30,7 +33,62 @@ const EventsPage: React.FC<EventsPageProps> = ({ events }) => {
 			>
 				Events
 			</Typography>
-			<Events events={events} />
+			{events.length > 0 ? (
+				<section className={classes("-section")}>
+					<Responsive.Row>
+						{events.map((event) => (
+							<Responsive.Col
+								key={event._id}
+								sm={100}
+								md={50}
+								lg={33}
+								xlg={33}
+							>
+								<div className={classes("-card")}>
+									<div
+										className={classes("-card-image")}
+										style={{
+											backgroundImage: `url(${event.image})`,
+										}}
+									/>
+									<div className={classes("-card-content")}>
+										<Typography
+											type="heading"
+											variant="title-1"
+											className={classes("-card-title")}
+										>
+											{event.name}
+										</Typography>
+										<Typography
+											type="body"
+											variant="large"
+											className={classes(
+												"-card-description"
+											)}
+										>
+											{event.description}
+										</Typography>
+										<Button
+											variant="fill"
+											className={classes("-card-button")}
+										>
+											Join Event Now
+										</Button>
+									</div>
+								</div>
+							</Responsive.Col>
+						))}
+					</Responsive.Row>
+				</section>
+			) : (
+				<Typography
+					type="heading"
+					variant="subtitle"
+					className={classes("-title")}
+				>
+					No events found
+				</Typography>
+			)}
 		</main>
 	);
 };
