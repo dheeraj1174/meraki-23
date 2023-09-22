380044c80557fb522300f8d0375d98e0e7e0fc1c
Akshat Mittal <akshatmittal2506@gmail.com>
2023-09-22 15:12:45 +0530

    fix: fixed team page

--- components/Team/Card/index.tsx
+++ components/Team/Card/index.tsx
@@ -3,6 +3,7 @@ import { IPerson } from "@/types/people";
 import TeamCardSocial from "./Social";
 import { stylesConfig } from "@/utils/functions";
 import styles from "./styles.module.scss";
+import Typography from "@/library/Typography";
 
 const classes = stylesConfig(styles, "team-card");
 
@@ -18,47 +19,44 @@ const TeamCard: React.FC<TeamCardProps> = ({
 	email,
 }) => {
 	return (
-		<div className={classes("")}>
-			<div
-				className={classes("-front")}
-				style={{
-					backgroundImage: `
-					    linear-gradient(
-							to bottom,
-							rgba(0, 0, 0, 0),
-						    rgba(0, 0, 0, 0.5),
-                            rgba(0, 0, 0, 0.8)
-						), url(${image})
-					`,
-				}}
+		<div
+			className={classes("")}
+			style={{
+				backgroundImage: `url(${image})`,
+			}}
+		>
+			<Typography
+				type="heading"
+				variant="title-2"
+				className={classes("-cover-name")}
+				data-aos="fade-up"
 			>
-				<span className={classes("-front-name")} data-aos="fade-up">
+				{name}
+			</Typography>
+			<div className={classes("-content")}>
+				<Typography
+					type="heading"
+					variant="title-2"
+					className={classes("-name")}
+					data-aos="fade-up"
+				>
 					{name}
-				</span>
-			</div>
-			<div
-				className={classes("-back")}
-				style={{
-					backgroundImage: `linear-gradient(
-                    105deg,
-                    rgba(0, 0, 0, 0.9) 0%,
-                    rgba(0, 0, 0, 0.9) 60%,
-                    transparent 50%
-                ),
-                url(${image})`,
-				}}
-			>
-				<div className={classes("-back-content")}>
-					<h3 className={classes("-name")}>{name}</h3>
-					<p className={classes("-bio")}>{bio}</p>
-					<div className={classes("-socials")}>
-						<TeamCardSocial
-							twitter={twitter}
-							instagram={instagram}
-							linkedin={linkedin}
-							email={email}
-						/>
-					</div>
+				</Typography>
+				<Typography
+					type="body"
+					variant="large"
+					className={classes("-bio")}
+					data-aos="fade-up"
+				>
+					{bio}
+				</Typography>
+				<div className={classes("-socials")} data-aos="fade-up">
+					<TeamCardSocial
+						twitter={twitter}
+						instagram={instagram}
+						linkedin={linkedin}
+						email={email}
+					/>
 				</div>
 			</div>
 		</div>
